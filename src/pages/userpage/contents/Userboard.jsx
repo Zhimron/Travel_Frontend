bb5add124ac5d660dbffbf2aaf0067e429df6aab
Shim <Shim@DESKTOP-00N04SV>
2023-07-07 00:05:26 +0800

    edit the output

--- src/pages/userpage/contents/Userboard.jsx
+++ src/pages/userpage/contents/Userboard.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Buttons from "../../../components/Button";
 
 const Boards = () => {
   const [data, setData] = useState(null);
@@ -24,19 +25,27 @@ const Boards = () => {
 
   return (
     <>
-      <div className=" bg-red-500 ">
-        {data &&
-          data.map((item) => (
-            <div
-              key={item._id}
-              className="bg-green-200 w-56 p-5 mt-1 flex flex-col items-start"
-            >
-              <p>PLACE:{item.place}</p>
-              <p>From: {item.from}</p>
-              <p>To: {item.to}</p>
-              <p>Experience: {item.experience}</p>
-            </div>
-          ))}
+      <div className="flex ">
+        <div className="ml-[115px]">
+          <h1 className="font-[font2] text-[40px] flex">Your Travel Experience</h1>
+          <div className="flex justify-start ">
+            {data &&
+              data.map((item) => (
+                <div key={item._id} className="flex m-3 bg-slate-400">
+                  <div className="p-5 inline-block text-left">
+                    <p className="font-[font2] font-bold text-lg">
+                      {item.place}
+                    </p>
+                    <p className="mt-2">
+                      <span className="font-[font2] font-bold text-lg">From:</span> {item.from} <span className="font-[font2] font-bold text-lg">To:</span> {item.to}
+                    </p>
+                    <span className="font-[font2] font-bold text-lg mt-4">Experience:</span>
+                    <p className="mt-2">{item.experience}</p>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
       </div>
     </>
   );
